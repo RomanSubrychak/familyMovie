e1315d5448728ec35345a36c4faf2f7dada4c945
Donny Wals <donnywals@gmail.com>
2016-07-30 12:31:59 +0200

    Implemented initial app flow

---
+++ FamilyMovies/FamilyMembersViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FamilyMembersViewController.swift
+//  FamilyMovies
+//
+//  Created by Donny Wals on 30/07/16.
+//  Copyright © 2016 DonnyWals. All rights reserved.
+//
+
+import UIKit
+
+class FamilyMembersViewController: UIViewController, AddFamilyMemberDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    func saveFamilyMember(withName name: String) {
+        print("should save family member with name: \(name)")
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let selectedIndex = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: selectedIndex, animated: true)
+        }
+        
+        if let navVC = segue.destinationViewController as? UINavigationController,
+            let addFamilyMemberVC = navVC.viewControllers[0] as? AddFamilyMemberViewController {
+            
+            addFamilyMemberVC.delegate = self
+        }
+    }
+
+}
+
+extension FamilyMembersViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FamilyMemberCell")
+            else { fatalError("Wrong cell identifier requested") }
+        
+        return cell
+    }
+}
